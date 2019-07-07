1db9a05f7da9472dfae2c369a24b01ac8da222cd
vmmadathil <madathilvisakh@gmail.com>
2019-07-07 16:20:17 -0500

    whole new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,40 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app.routing';
 
 import { AppComponent } from './app.component';
-import { NavbarComponentComponent } from './navbar-component/navbar-component.component';
-import { LoginComponent } from './login/login.component';
-import { LoginService } from './shared/repositories/login.service';
-
-import { HttpClient , HttpHeaders} from '@angular/common/http';
-import { HttpClientModule } from '@angular/common/http';
-import { NameButtonComponent } from './name-button/name-button.component';
-import { SignUpComponent } from './sign-up/sign-up.component';
+import { SignupComponent } from './signup/signup.component';
 import { LandingComponent } from './landing/landing.component';
+import { ProfileComponent } from './profile/profile.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+
+import { HomeModule } from './home/home.module';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, HttpClientModule],
-  declarations: [ AppComponent, NavbarComponentComponent, LoginComponent, NameButtonComponent, SignUpComponent, LandingComponent],
-  bootstrap:    [ AppComponent ],
-  providers: [LoginService, HttpClient]
+  declarations: [
+    AppComponent,
+    SignupComponent,
+    LandingComponent,
+    ProfileComponent,
+    NavbarComponent,
+    FooterComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    NgbModule.forRoot(),
+    FormsModule,
+    RouterModule,
+    AppRoutingModule,
+    HomeModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-landing',
-  templateUrl: './landing.component.html',
-  styleUrls: ['./landing.component.css']
+    selector: 'app-landing',
+    templateUrl: './landing.component.html',
+    styleUrls: ['./landing.component.scss']
 })
+
 export class LandingComponent implements OnInit {
+  focus: any;
+  focus1: any;
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
 }
